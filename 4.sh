53d7038bd8831893c27372279b60e6f5389a6008
AngelSecurityTeam <55148534+AngelSecurityTeam@users.noreply.github.com>
2020-03-31 17:10:54 -0500

    Delete 4.sh

--- 4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#https://github.com/AngelSecurityTeam
-
-case $1 in
-         
-	-cert ) curl -s "https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=$2"|jq .subdomains|grep -o "\w.*$2"
-esac
