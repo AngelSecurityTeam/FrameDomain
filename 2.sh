25214904f76683f29e5c58df0f14b21ecc9df433
AngelSecurityTeam <55148534+AngelSecurityTeam@users.noreply.github.com>
2020-03-31 17:10:27 -0500

    Delete 2.sh

--- 2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#https://github.com/AngelSecurityTeam
-
-case $1 in
-     
-	-cert ) curl -s https://certspotter.com/api/v0/certs\?domain\=$2 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep $2
-
-esac
