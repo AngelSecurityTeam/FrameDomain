59132b3a74ad697241078fc5090a7977116d876c
AngelSecurityTeam <55148534+AngelSecurityTeam@users.noreply.github.com>
2020-03-31 17:10:39 -0500

    Delete 3.sh

--- 3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#https://github.com/AngelSecurityTeam
-
-case $1 in
-         
-	-cert ) curl -s "https://dns.bufferover.run/dns?q=."$2 | jq -r .FDNS_A[]|cut -d',' -f2|sort -u | grep $2
-esac
