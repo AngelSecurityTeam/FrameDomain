4747f29760f6d93cfa8a627561c3d955e26161b4
AngelSecurityTeam <55148534+AngelSecurityTeam@users.noreply.github.com>
2020-04-28 12:53:00 -0500

    Update 2.py

--- module/2.py
+++ module/2.py
@@ -1,25 +1,10 @@
-
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
 #https://github.com/AngelSecurityTeam/SubDomainExt
 
 import requests
-print("""
-\033[1;35m
- _____       _    ______                      _       _____     _   
-/  ___|     | |   |  _  \                    (_)     |  ___|   | |  
-\ `--. _   _| |__ | | | |___  _ __ ___   __ _ _ _ __ | |____  _| |_ 
- `--. \ | | | '_ \| | | / _ \| '_ ` _ \ / _` | | '_ \|  __\ \/ / __|
-\033[1;39m/\__/ / |_| | |_) | |/ / (_) | | | | | | (_| | | | | | |___>  <| |_ \033[1;35m
-\033[1;39m\____/ \__,_|_.__/|___/ \___/|_| |_| |_|\__,_|_|_| |_\____/_/\_\\__|\033[1;35m
-                                                 AngelSecurityTeam                                                    
-""")                                                                    
-                                                                    
-
-
-
-website = input("Website : \033[1;39m")
-print ("")
+website = input("\n\033[1;39mWebsite : \033[1;39m")
+print ("\033[1;39m")
 
 def main(website):
 	url = "https://api.indoxploit.or.id/domain/{}".format(website)
